182cf83ef088d074e017a7ee3da4ed71f741035c
Tomwey <tomwey@163.com>
2016-11-26 21:17:20 +0800

    update channel backend

--- app/admin/channel.rb
+++ app/admin/channel.rb
@@ -13,5 +13,25 @@ permit_params :name, :intro, :image, :live_url, :sort, :opened
 #   permitted
 # end
 
+index do
+  selectable_column
+  column('ID',:id) { |channel| link_to channel.id, cpanel_channel_path(channel) }
+  column(:name, sortable: false) { |channel| link_to channel.name, cpanel_channel_path(channel) }
+  column :image, sortable: false do |channel|
+    if channel.image.blank?
+      ''
+    else
+      image_tag channel.image.url(:small)
+    end
+  end
+  column :intro, sortable: false
+  column :sort
+  column :opened
+  column :created_at
+  
+  actions
+  
+end
+
 
 end
