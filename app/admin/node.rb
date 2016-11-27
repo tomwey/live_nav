5d8bd5b6a9feeb435d6c71ec92379eba751f650b
Tomwey <tomwey@163.com>
2016-11-27 15:16:52 +0800

    ccc

--- app/admin/node.rb
+++ app/admin/node.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Node do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :sort
 #
 # or
 #
@@ -12,6 +12,14 @@ ActiveAdmin.register Node do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+form do |f|
+  f.inputs do
+    f.input :name
+    f.input :sort
+  end
+  actions
+end
 
 
 end
