12fd1e200d97edd7b48297bae86401aff39d57b7
tomwey <tomwey@163.com>
2016-12-07 09:38:27 +0800

    do fetch douyu func

--- fetch_douyu.rb
+++ fetch_douyu.rb
@@ -3,7 +3,7 @@ require 'digest'
 
 module Douyu
   class Room
-    def self.fetch(room_id)
+    def self.fetch_room_info(room_id)
       time = Time.now.to_i
       
       auth = Digest::MD5.hexdigest("lapi/live/thirdPart/getPlay/#{room_id}?aid=hbtv&time=#{time}rRrJQ10J8cnGmAz2")
@@ -17,9 +17,28 @@ module Douyu
       end
       
     end
+    
+    # 获取正在直播的房间列表
+    def self.fetch_room_list
+      time = Time.now.to_i
+      
+      auth = Digest::MD5.hexdigest("api/thirdPart/live?aid=hbtv&limit=10&offset=0&time=#{time}rRrJQ10J8cnGmAz2")
+      
+      url = "http://coapi.douyucdn.cn/api/thirdPart/live?aid=hbtv&time=#{time}&auth=#{auth}&limit=10&offset=0"
+      puts url
+      
+      RestClient.get url, { aid: 'hbtv', time: time, auth: auth, accept: :json } do |resp|
+        puts JSON.parse(resp)["data"]
+        # puts '---------------------------------------------'
+      end
+    end
+    
   end
 end
 
-%w(321987 265593 683954 1339207 1235257 479171 1243456 8303 966276 542173).each do |room_id|
-  Douyu::Room.fetch(room_id)
-end
+# %w(321987 265593 683954 1339207 1235257 479171 1243456 8303 966276 542173).each do |room_id|
+#   Douyu::Room.fetch(room_id)
+# end
+
+# Douyu::Room.fetch_room_list
+Douyu::Room.fetch_room_info(442078)
