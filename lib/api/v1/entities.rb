07b7f6a9e88b145e88bad346f427268b469f1540
Tomwey <tomwey@163.com>
2017-03-10 15:46:40 +0800

    dsa

--- lib/api/v1/entities.rb
+++ lib/api/v1/entities.rb
@@ -74,7 +74,7 @@ module API
         expose :view_count
         # expose :intro, format_with: :null
         expose :title do |model, opts|
-          model.current_playlist.try(:name) || '该频道正在播放的节目名称'
+          model.current_playlist.try(:name) || ''
         end
         expose :image do |model, opts|
           # model.image.blank? ? '' : model.image.url(:thumb)
