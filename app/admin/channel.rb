d5a5df50c2b600d55fb22142760ed40b72ecfa13
Tomwey <tomwey@163.com>
2017-03-10 14:55:52 +0800

    ccc

--- app/admin/channel.rb
+++ app/admin/channel.rb
@@ -18,11 +18,7 @@ index do
   column('ID',:id) { |channel| link_to channel.id, cpanel_channel_path(channel) }
   column(:name, sortable: false) { |channel| link_to channel.name, cpanel_channel_path(channel) }
   column :image, sortable: false do |channel|
-    if channel.image.blank?
-      ''
-    else
-      image_tag channel.image.url(:small)
-    end
+    channel.real_image
   end
   column '所属节点' do |channel|
     raw("#{channel.nodes.map { |node| node.name }.join('<br>')}")
