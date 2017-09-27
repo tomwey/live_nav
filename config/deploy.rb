11315db51e16861c32c9d45197c2ed0e88911b30
tomwey <tomwey@163.com>
2017-09-27 17:33:16 +0800

    change sidekiq config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for Capistrano 3.1
-lock '3.7.2'
+# lock '3.7.2'
 
 set :application, 'live_nav'
 set :deploy_user, "deployer"
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -11,17 +11,17 @@ redis_config = YAML.load_file("#{Rails.root}/config/redis.yml")[Rails.env]
 $redis = Redis.new(host: redis_config['host'], port: redis_config['port'])
 Redis::Objects.redis = $redis
 
-sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/0"
+sidekiq_url = "redis://#{redis_config['host']}:#{redis_config['port']}/1"
 Sidekiq.configure_server do |config|
   config.redis = { namespace: 'sidekiq', url: sidekiq_url }
   
-  # 添加cron jobs
-  schedule_file = "config/schedule.yml"
-  # puts 'sssssss'
-  if File.exists?(schedule_file) && Sidekiq.server?
-    # puts 'ddddddd'
-    Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-  end
+  # # 添加cron jobs
+  # schedule_file = "config/schedule.yml"
+  # # puts 'sssssss'
+  # if File.exists?(schedule_file) && Sidekiq.server?
+  #   # puts 'ddddddd'
+  #   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+  # end
 end
 Sidekiq.configure_client do |config|
   config.redis = { namespace: 'sidekiq', url: sidekiq_url }
